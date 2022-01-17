e7420e932cfaa10490fe8035044c11784ac15d48
MateusJSouza <mateusjdev@gmail.com>
2022-01-17 14:52:57 -0300

    01-Counter

--- DecreaseButton/script.js
+++ DecreaseButton/script.js
@@ -10,16 +10,30 @@ console.log(btns)
 // forEach -> executa uma função a cada elemento de um array
 btns.forEach(function(btn) {
   btn.addEventListener('click', (e) => {
-   const styles = e.currentTarget.classList;
-   // se a minha variável contém a classe...
-   if (styles.contains('decrease')) {
-     count--;
-   }
-   else if (styles.contains('increase')) {
-     count++;
-   }
-   else count = 0;
-   value.textContent = count;
+    const styles = e.currentTarget.classList;
+    // se a minha variável contém a classe...
+    if (styles.contains('decrease')) {
+      count--;
+    }
+    else if (styles.contains('increase')) {
+      count++;
+    }
+    else {
+      count = 0;
+    }
+
+    if (count > 0) {
+      value.style.color = 'green'
+    }
+
+    if (count < 0) {
+      value.style.color = 'red'
+    }
+
+    if (count === 0) {
+      value.style.color = '#222'
+    }
+    value.textContent = count;
   })
 })
 
